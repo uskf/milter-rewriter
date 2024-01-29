6f57df1bcfaf8db1d7816d1cdbe76450b3e087b9
Yusuke Fujimaki <usk.fujimaki@gmail.com>
2024-01-30 03:47:45 +0900

    Add Rule file loader,Reverse map and Rewriting header

--- milter-rewriter.py
+++ milter-rewriter.py
@@ -4,6 +4,8 @@ import os
 import re
 import Milter
 from Milter.utils import *
+from yaml import safe_load
+
 
 class MilterRewriter(Milter.Base):
     "Milter for Mail Rewrite"
@@ -14,36 +16,75 @@ class MilterRewriter(Milter.Base):
         self.fromheader = None
         self.messageid = None
         self.pid = os.getpid()
-        self.queueid = None
-        self.rewrite = None
-
+        self.status = []
+        self.__reset_status()
 
 
     @Milter.noreply
     def envfrom(self, f, *str):
         self.mailfrom = f
         (_displayname, self.envfromaddr) = parseaddr(f)
-        if re.fullmatch("foo@example\.org",self.envfromaddr):
-            self.rewrite = True
+        self.__header_search("EnvFrom", f)
         return Milter.CONTINUE
-
 
 
     @Milter.noreply
     def header(self, field, value):
-        if re.fullmatch("From", field):
-            self.fromheader = value
+        self.__header_search(field, value)
         return Milter.CONTINUE
 
 
-
     def eom(self):
-        if self.rewrite:
-            self.addheader("Cc", self.fromheader, 0)
-            self.chgheader("From", 0, "admin@example.org")
+        queueid = self.getsymval('i')
+        for num, check_restult in enumerate(self.status):
+            if all(check_restult):
+                for rwt in rule_definition[num]["rewrites"]:
+                    if rwt['action'] == "addheader":
+                        self.addheader(rwt["field"], rwt['value'], 0)
+                        print("[{0}] add header {1}:{2}".format(queueid, rwt["field"], rwt["value"]))
+                    elif rwt['action'] == "chgheader":
+                        self.chgheader(rwt["field"], 0, rwt['value'])
+                        print("[{0}] change header {1}:{2}".format(queueid, rwt["field"], rwt["value"]))
+                self.__reset_status()
+                return Milter.ACCEPT
+
+        print("[{0}] No rewrite rule matched".format(queueid))
+        self.__reset_status()
         return Milter.ACCEPT
 
+
+    def __reset_status(self):
+        self.status = []
+        for r in rule_definition:
+            self.status.append([False]*len(r['conditions']))
+
+
+    def __header_search(self, fieldname, value):
+        if fieldname in reverse_map:
+            for p in reverse_map[fieldname]:
+                if p.search(value):
+                    for r,c in reverse_map[fieldname][p]:
+                        self.status[r][c]=True
+
+
+def init_reversemap(rules):
+    reversemap = {}
+    for r, rule in enumerate(rules):
+        for c, cond in enumerate(rule["conditions"]):
+            fieldname = list(cond.keys())[0]
+            pattern = list(cond.values())[0]
+            compiled = re.compile(pattern)
+            if fieldname not in reversemap:
+                reversemap[fieldname]={}
+            if compiled in reversemap[fieldname]:
+                reversemap[fieldname][compiled].append([r,c])
+            else:
+                reversemap[fieldname][compiled]=[[r,c]]
+    return(reversemap)
+
+
 def main():
+
     if not os.path.exists('/var/spool/postfix/milter-rewriter'):
         os.mkdir('/var/spool/postfix/milter-rewriter', mode=0o777)
 
@@ -57,6 +98,11 @@ def main():
     # shutdown
     os.remove('/run/milter-rewriter/milter-rewriter.pid')
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == '__main__':
+    yamlfile = open('/etc/rewrite-rules.yaml', mode="r")
+    rule_definition = safe_load(yamlfile)
+    yamlfile.close()
+    reverse_map = init_reversemap(rule_definition)
+
+    main()
